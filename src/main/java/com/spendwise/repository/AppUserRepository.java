9f20c597970dc0fca467f4e7b7c2c107c78b4446
chetan <jangidchetan.jpr@gmail.com>
2026-09-19 19:34:30 +0530

    Update AppUserRepository

--- src/main/java/com/spendwise/repository/AppUserRepository.java
+++ src/main/java/com/spendwise/repository/AppUserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AppUserRepository extends JpaRepository<AppUser, UUID> {
   Optional<AppUser> findByEmail(String email);
-  boolean existsByEmail(String email);
+    boolean existsByEmail(String email);
 }
